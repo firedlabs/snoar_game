618dd44c1aafda6c35c589bcb846c9df57e17def
Marco Bruno <marco.bruno@meliuz.com.br>
2022-01-24 12:44:20 +0100

    :construction: Create a login simple page

--- scenes/cities/pakoskoya/pakoskoya.go
+++ scenes/cities/pakoskoya/pakoskoya.go
@@ -13,3 +13,18 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "pakoskoya1"
+  component: "/scenes/cities/pakoskoya/pakoskoya.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
